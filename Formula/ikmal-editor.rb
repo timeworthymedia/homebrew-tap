083574ccf57fbb8a0d2c177b385441487a7b2ffd
Ian Sherr <ian@timeworthymedia.com>
2026-08-03 16:00:04 -0400

    fix: align tap hashes with main release binaries

--- Formula/ikmal-editor.rb
+++ Formula/ikmal-editor.rb
@@ -6,13 +6,13 @@ class IkmalEditor < Formula
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/timeworthymedia/ikmal-editor/releases/download/v0.9.0-beta/ikmal-editor-v0.9.0-beta-darwin-arm64.tar.gz"
-    sha256 "b0a0dfc88d0adf26f772052ecce595cd06253cbd3c559590e42774cb69caad96"
+    sha256 "0c0110eb3e250f6dc7ccff9c25a86a23810c7280f3beb301b4748de83a7907d0"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/timeworthymedia/ikmal-editor/releases/download/v0.9.0-beta/ikmal-editor-v0.9.0-beta-darwin-amd64.tar.gz"
-    sha256 "3e98295b376d0aedf3c7546ea7933742d8819e4d6e301e7a80ee59dc91e77c10"
+    sha256 "9dec56604a3a5780ddac4fd9bb5cc5d9bf38cef6e6f114fc68c1f5f33f35aa5c"
   elsif OS.linux?
     url "https://github.com/timeworthymedia/ikmal-editor/releases/download/v0.9.0-beta/ikmal-editor-v0.9.0-beta-linux-amd64.tar.gz"
-    sha256 "cc88134b0359537e3ed26807d267e738859df58e3380858470a0ba043a720d44"
+    sha256 "bb2eb5c968f6293e34b7c53112afb2248492c233d501d078ef22cc6dbfefa3e9"
   end
 
   def install
